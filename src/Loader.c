62102740778d6f75cd0ab7a12198154696e3b98e
loulei <constantlike@gmail.com>
2018-06-02 17:31:31 +0800

    add soinfo

--- src/Loader.c
+++ src/Loader.c
@@ -1,1 +1,39 @@
 #include "Loader.h"
+
+typedef struct soinfo_pool_t soinfo_pool_t;
+#define SOINFO_PER_POOL ((PAGE_SIZE - sizeof(soinfo_pool_t*)) / sizeof(soinfo))
+
+struct soinfo_pool_t {
+	soinfo_pool_t *next;
+	soinfo info[SOINFO_PER_POOL];
+};
+
+static struct soinfo_pool_t *gSoInfoPools = NULL;
+static soinfo *gSoInfoFreeList = NULL;
+
+static soinfo *solist = NULL;
+static soinfo *sonext = NULL;
+static soinfo *somain;
+
+static const char *const gSoPaths[] = {
+		"/vendor/lib",
+		"/system/lib",
+		NULL
+};
+
+
+soinfo *do_dlopen(const char *name, int flags){
+	if((flags & ~(RTLD_NOW | RTLD_LAZY | RTLD_LOCAL | RTLD_GLOBAL)) != 0){
+		LOGE("invalid flags to dlopen: %x", flags);
+		return NULL;
+	}
+
+}
+
+static void set_soinfo_pool_protection(int protection){
+	for(soinfo_pool_t *p = gSoInfoPools; p != NULL; p = p->next){
+		if(mprotect(p, sizeof(*p), protection) == -1){
+			abort();
+		}
+	}
+}
--- src/Loader.h
+++ src/Loader.h
@@ -11,7 +11,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <elf.h>
-
+#include <stdbool.h>
+#include <dlfcn.h>
+#include <sys/mman.h>
+#include "common.h"
 
 
 
@@ -33,6 +36,18 @@
 
 typedef struct _soinfo soinfo;
 
+typedef void (*linker_function_t)();
+
+typedef struct _link_map_t link_map_t;
+
+struct _link_map_t {
+	uintptr_t l_addr;
+	char *l_name;
+	uintptr_t l_ld;
+	link_map_t *l_next;
+	link_map_t *l_prev;
+};
+
 struct _soinfo{
 	char name[SOINFO_NAME_LEN];
 	const Elf32_Phdr *phdr;
@@ -49,6 +64,57 @@ struct _soinfo{
 	uint32_t unused3;
 
 	soinfo *next;
+	unsigned flags;
+
+	const char *strtab;
+	Elf32_Sym *symtab;
+
+	size_t nbucket;
+	size_t nchain;
+
+	unsigned *bucket;
+	unsigned *chain;
+
+	unsigned *plt_got;
+
+	Elf32_Rel *plt_rel;
+	size_t plt_rel_count;
+
+	Elf32_Rel *rel;
+	size_t rel_count;
+
+	linker_function_t *preinit_array;
+	size_t preinit_array_count;
+
+	linker_function_t *init_array;
+	size_t init_array_count;
+	linker_function_t *fini_array;
+	size_t fini_array_count;
+
+	linker_function_t init_func;
+	linker_function_t fini_func;
+
+	unsigned *ARM_exidx;
+	size_t ARM_exidx_count;
+
+	size_t ref_count;
+	link_map_t link_map;
+
+	bool constructors_called;
+
+	// When you read a virtual address from the ELF file, add this
+	// value to get the corresponding address in the process' address space.
+	Elf32_Addr load_bias;
+
+	bool has_text_relocations;
+	bool has_DT_SYMBOLIC;
+
+	void CallConstructors();
+	void CallDestructors();
+	void CallPreInitConstructors();
+
+	void CallArray(const char* array_name, linker_function_t* functions, size_t count, bool reverse);
+	void CallFunction(const char* function_name, linker_function_t function);
 };
 
 extern soinfo libdl_info;
